98e3384cde7cc5cad7c898dd2525b7e301474168
Beltrancantero <canteropuigbeltran@gmail.com>
2025-02-17 10:33:55 +0100

    arreglo de una errata

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express =require("express");
 const app = express();
+const morgan = require("morgan")
 
 app.set("port", process.env.PORT || 8080);
 
 //Routes
 app.use(require("./routes/index"));
+
+app.use(morgan("dev"))
 
 //Iniciando el servidor
 app.listen(app.get("port"), ()=> {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,35 @@
 const { Router } = require("express");
 const router = Router ();
 
+//rutas POST
+
+router.post("/rutepost",  (req, res)  => {
+    console.log("hola en la consola de node, desde una llamada en el servidor");
+    res.send("hola desde una web llamada en el servidor");
+});
+
+router.post("/rutepostInversa",  (req, res)  => {
+    console.log("rodivres le ne adamall arto edsed ,edon ed alosnoc al ne aloh");
+    res.send("rodivres le ne adamall bew anu edsed aloh");
+});
+
+router.post("/rutepostJSON",  (req, res)  => {
+    console.log("hola en la consola de node, desde otra llamada en el servidor");
+    res.json("hola desde otra web llamada en el servidor pero esta vez con JSON");
+});
+
+router.post("/rutepostMacarena",  (req, res)  => {
+    console.log("hola en la consola de node, desde la pista de baile");
+    res.send("dale a tu cuerpo alegria macarena");
+});
+
+router.post("/rutepost47",  (req, res)  => {
+    console.log("hola en la consola de node, desde el numero 47");
+    res.send("47");
+});
 
 //Rutas GET
-router.get("/ruteGet",  (req, res)  => {
+router.Get("/ruteGet",  (req, res)  => {
     console.log("hola en la consola de node, desde una llamada en el servidor");
     res.send("hola desde una web llamada en el servidor");
 });
@@ -27,4 +53,6 @@ router.get("/ruteGet47",  (req, res)  => {
     console.log("hola en la consola de node, desde el numero 47");
     res.send("47");
 });
+
+
 module.exports = router;
